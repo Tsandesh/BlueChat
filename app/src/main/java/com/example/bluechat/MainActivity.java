3b843a8a0fb10e0bf4353c1e1e5423bee808ee86
Tsandesh <uchihazoro02@gmail.com>
2022-01-16 20:12:12 +0545

    location permission and paired device list donee hai
    device list

--- app/src/main/java/com/example/bluechat/MainActivity.java
+++ app/src/main/java/com/example/bluechat/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.bluechat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,6 +19,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int LOCATION_PERMISSION_REQUEST = 101;
     private Context context;
     private BluetoothAdapter bluetoothAdapter;
 
@@ -45,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.menu_search_devices:
-                Toast.makeText(context, "Clicked Search Devices", Toast.LENGTH_SHORT).show();
+                checkPermissions();
                 return true;
 
             case R.id.menu_enable_bluetooth:
@@ -57,11 +62,54 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST);
+        }else{
+            Intent intent = new Intent(context,DeviceListActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permission, @NonNull int[] grantResults) {
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == getPackageManager().PERMISSION_GRANTED) {
+                Intent intent = new Intent(context, DeviceListActivity.class);
+                startActivity(intent);
+            } else {
+                new AlertDialog.Builder(context)
+                        .setCancelable(false)
+                        .setMessage("Location Permission in Required \n Please Grant it.")
+                        .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permission, grantResults);
+        }
+
+    }
+
     private void enableBluetooth() {
         if (bluetoothAdapter.isEnabled()) {
             Toast.makeText(context, "Bluetooth Already Enabled", Toast.LENGTH_SHORT).show();
         } else {
 
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
             bluetoothAdapter.enable();
         }
     }
