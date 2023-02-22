481f116ea3dc79913e8383ea2b5b777dbaa4cea8
Tsandesh <uchihazoro02@gmail.com>
2023-02-22 21:08:18 +0545

    Listing all nearby devices pfffff and small changes in xml

--- app/src/main/java/com/example/bluechat/DeviceListActivity.java
+++ app/src/main/java/com/example/bluechat/DeviceListActivity.java
@@ -1,22 +1,32 @@
 package com.example.bluechat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.util.Set;
 
 public class DeviceListActivity extends AppCompatActivity {
 
     private ListView listPairedDevices, listAvailableDevices;
+    private ProgressBar progressScanDevices;
 
     private ArrayAdapter<String> adapterPairedDevices, adapterAvailableDevices;
     private Context context;
@@ -33,6 +43,7 @@ public class DeviceListActivity extends AppCompatActivity {
     private void init() {
         listPairedDevices = findViewById(R.id.list_paired_devices);
         listAvailableDevices = findViewById(R.id.list_available_devices);
+        progressScanDevices = findViewById(R.id.progress_scan_devices);
 
         adapterPairedDevices = new ArrayAdapter<String>(context, R.layout.device_list_item);
         adapterAvailableDevices = new ArrayAdapter<String>(context, R.layout.device_list_item);
@@ -52,11 +63,84 @@ public class DeviceListActivity extends AppCompatActivity {
             return;
         }
         Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        if(pairedDevices != null && pairedDevices.size() >0){
-            for(BluetoothDevice device : pairedDevices ){
-                adapterPairedDevices.add(device.getName()+ "\n"+device.getAddress());
+        if (pairedDevices != null && pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                adapterPairedDevices.add(device.getName() + "\n" + device.getAddress());
             }
         }
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bluetoothDeviceListener, intentFilter);
+        IntentFilter intentFilter1 = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(bluetoothDeviceListener, intentFilter1);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_device_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private BroadcastReceiver bluetoothDeviceListener = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
+                    adapterAvailableDevices.add(device.getName() + "\n" + device.getAddress());
+                }
+            }else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+                progressScanDevices.setVisibility(View.GONE);
+                if(adapterAvailableDevices.getCount() ==0){
+                    Toast.makeText(context,"No new Devices found",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(context,"Click on device to start the chat",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_scan_devices:
+                scanDevices();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void scanDevices() {
+        progressScanDevices.setVisibility(View.VISIBLE);
+        adapterAvailableDevices.clear();
+        Toast.makeText(context,"Scan started", Toast.LENGTH_SHORT).show();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if (bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+        }
+        bluetoothAdapter.startDiscovery();
     }
 }
--- app/src/main/java/com/example/bluechat/MainActivity.java
+++ app/src/main/java/com/example/bluechat/MainActivity.java
@@ -112,5 +112,10 @@ public class MainActivity extends AppCompatActivity {
             }
             bluetoothAdapter.enable();
         }
+        if(bluetoothAdapter.getScanMode()!=BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE){
+            Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300);
+            startActivity(discoveryIntent);
+        }
     }
 }
